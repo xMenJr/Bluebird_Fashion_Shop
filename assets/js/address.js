28bdf98755e1ea83eb50763378241b25c5970271
xMenJr <20212181@eaut.edu.vn>
2023-09-20 22:07:12 +0700

    up file

---
+++ assets/js/address.js
@@ -0,0 +1,31 @@
+// Update
+const update = document.querySelector('.update')
+const show = document.querySelector('.add_update')
+const addressUpdate = document.querySelector('.address_update')
+const overlay = document.querySelector('.overlay')
+
+update.addEventListener('click', function() {
+    updateAddress.innerHTML = 'Update address'
+    addressUpdate.innerHTML = 'Update'
+    show.style.display = 'block'
+    overlay.style.display = 'block'
+})
+
+const exit = document.querySelector('.exit')
+
+exit.addEventListener('click', function() {
+    show.style.display = 'none'
+    overlay.style.display = 'none'
+})
+
+
+// Add_newaddress
+const addnewAddress = document.querySelector('.add_newaddress')
+const updateAddress = document.querySelector('.update_address')
+
+addnewAddress.addEventListener('click', function() {
+    updateAddress.innerHTML = 'Add a new address'
+    addressUpdate.innerHTML = 'Add'
+    overlay.style.display = 'block'
+    show.style.display = 'block'
+})
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,12 +1,27 @@
 const show = document.querySelector('.login_showPass--icon')
 
+const hide = document.querySelector('.login_hidePass--icon')
+
 const pass = document.querySelector('#passWord')
+
 
 show.addEventListener('click', function() {
     if(pass.type == 'text') {
         pass.type = 'password'
+        show.style.display = 'none'
+        hide.style.display = 'block'
     }
     else {
         pass.type = 'text'
+        show.style.display = 'block'
+        hide.style.display = 'none'
+    }
+})
+
+hide.addEventListener('click', function() {
+    if(pass.type == 'password') {
+        pass.type = 'text'
+        show.style.display = 'block'
+        hide.style.display = 'none'
     }
 })
